d77e58883e45738bff7ee2479ee178d860260b56
matx3001 <matx3001@a48bac42-b2a7-46d1-93c9-c9e7c2af7f73>
2009-04-20 12:04:54 +0000

    dumbed down and minimal

--- examples/avoider/avoid.erl
+++ examples/avoider/avoid.erl
@@ -11,48 +11,37 @@
 
 %starts up a simple demo of 1 robot
 demo() ->
-	%adjust these to the correct path if needed
-	code:add_path("../../module/"),code:add_path("../../ebin/"),
-	init(rih:init( mrh:start(), 0)).
-
+    code:add_path("../../module/"),code:add_path("../../ebin/"),
+    init(rih:init( mrh:start(), 0)).
 
 init({error, Error}) ->
-	io:format("Error: ~p~n", [Error]);
+    io:format("Error: ~p~n", [Error]);
 
 %spawns the 2 required servers to run
 init(Rid) ->
-	Pid = spawn(?MODULE, travel, [Rid]),
-	spawn(?MODULE, collision, [Pid, Rid]),
-	Pid.
-
+    spawn(?MODULE, collision, [Pid = spawn(?MODULE, travel, [Rid]), Rid]),
+    Pid.
 
 % Moves the robot until a wall is found where it will be rotated
-travel(Rid) ->
-	
-	receive
-		{collision, true, Direction} ->
-			mvh:rotate(Rid, speed, 10/Direction);
-		{collision,false} ->
-			mvh:move(Rid, speed, 0.5)			
-	end,
-	travel(Rid).
+travel(Rid) ->	
+    receive
+	{collision, true} ->
+	    mvh:rotate(Rid, speed, 10);
+	{collision,false} ->
+	    mvh:move(Rid, speed, 0.5)			
+    end,
+    travel(Rid).
 
 
 % notifies the mover process if there are any walls or not
 collision(Pid, Rid) ->
-	{Left, Right} = split_lasers( dvh:read_lasers(Rid)),
-	is_collision(Pid, lists:min(lists:sublist(lists:reverse(Left),90)), lists:min(lists:sublist(Right,90))),
-	timer:sleep(500),
-	collision(Pid, Rid).
-
-split_lasers({_, Res}) ->
-	lists:split(erlang:trunc( string:len(Res)/2),Res).
-
+    {_, Results} = dvh:read_lasers(Rid),
+    is_collision(Pid, lists:min(Results)),
+    timer:sleep(500),
+    collision(Pid, Rid).
 
 % a simple collision detector
-is_collision(Pid, LeftMin, _) when LeftMin < 1 ->
-	Pid ! {collision, true, 1};
-is_collision(Pid, _, RightMin) when RightMin < 1 ->
-	Pid ! {collision, true, -1};
-is_collision(Pid, _, _) ->
-	Pid ! {collision, false}.
+is_collision(Pid, Min) when Min < 1 ->
+    Pid ! {collision, true};
+is_collision(Pid, _) ->
+    Pid ! {collision, false}.
